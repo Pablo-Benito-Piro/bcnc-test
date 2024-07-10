1cb330a9996ef71c1bf11c9720e63e6262cb64fa
Pablo-Benito-Piro <pablo.benito.piro@gmail.com>
2024-07-10 16:36:13 +0200

    initial commit
    creation of the project structure

---
+++ src/main/kotlin/com/pbp/bcnctest/BcncTestApplication.kt
@@ -0,0 +1,11 @@
+package com.pbp.bcnctest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BcncTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<BcncTestApplication>(*args)
+}
