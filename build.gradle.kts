c17ebf613c63c4b327e406c8a5838f9110d37d40
Pablo-Benito-Piro <pablo.benito.piro@gmail.com>
2024-07-10 16:47:38 +0200

    add Junit, mockito and test dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,11 +18,18 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-devtools")
-
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("io.mockk:mockk:1.10.4")
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         jvmTarget = "17"
     }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
